af5219cb1753f08e2848e070e7eed63730e7d5ef
hikmatullo <hikmatulloanvarbekov001@gmail.com>
2024-03-09 16:40:11 +0500

    docker compose done

--- loans/src/main/java/com/hikmatullo/loans/entity/Loans.java
+++ loans/src/main/java/com/hikmatullo/loans/entity/Loans.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Loans extends BaseEntity {
 
 	@Id
